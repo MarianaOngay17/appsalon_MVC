556d28227bbe5d8375cd89cc990a03ac259e82cb
Mariana <marianaongay.01@gmail.com>
2025-08-12 13:01:27 -0600

    Configuración inicial

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # appsalon_MVC
 Proyecto App Salon MVC - Curso Desarrollo Web
+
+Pasos iniciales:
+
+Instalar dependencias
+
+    npm install
+
+Ejecutar gulp
+
+    npm run dev
+
+Instalar composer
+
+    composer init
+
+Modificar archivo composer.json a lo siguiente
+
+    "autoload": {
+        "psr-4": {
+            "MVC\\": "./", 
+            "Controllers\\": "./controllers",
+            "Model\\": "./models"
+        }
+    },
+
+Actualizar archivo
+
+    composer update
+
+Compilar proyecto dentro de carpeta public
+    php -S localhost:3000
--- public/index.php
+++ public/index.php
@@ -3,8 +3,24 @@
 require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
+use Controllers\LoginController;
 
 $router = new Router();
+
+//iniciar sesión
+$router->get('/', [LoginController::class, 'login']);
+$router->post('/', [LoginController::class, 'login']);
+$router->get('/logout', [LoginController::class, 'logout']);
+
+//recuperar password
+$router->get('/olvide', [LoginController::class, 'olvide']);
+$router->post('/olvide', [LoginController::class, 'olvide']);
+$router->get('/recuperar', [LoginController::class, 'recuperar']);
+$router->post('/recuperar', [LoginController::class, 'recuperar']);
+
+//crear cuenta
+$router->get('/crear-cuenta', [LoginController::class, 'crear']);
+$router->post('/crear-cuenta', [LoginController::class, 'crear']);
 
 
 
