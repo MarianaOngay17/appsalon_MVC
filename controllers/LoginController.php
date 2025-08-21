87e768ddf9ee164d61dd9957d43ba496f50d5c5a
Mariana <marianaongay.01@gmail.com>
2025-08-21 12:57:19 -0600

    Autenticación de usuarios

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -9,8 +9,45 @@ use Classes\Email;
 class LoginController{
 
     public static function login(Router $router){
+        $alertas = [];
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            $auth = new Usuario($_POST);
+            $alertas = $auth->validarLogin();
+
+            if(empty($alertas)){
+                //comprobar que exista
+                $usuario = Usuario::where('email', $auth->email);
+                
+                if($usuario){
+                    //verificar password y confirmado
+                    if($usuario->comprobarPasswordAndVerificado($auth->password)){
+                        session_start();
+                        $_SESSION['id'] = $usuario->id;
+                        $_SESSION['nombre'] = $usuario->nombre . " " . $usuario->apellido;
+                        $_SESSION['email'] = $usuario->email;
+                        $_SESSION['login'] = true;
+
+                        //redireccionamiento
+                        if($usuario->admin === "1"){
+                            $_SESSION['admin'] = $usuario->admin ?? null;
+                            header('Location: /admin');
+                        }else{
+                            header('Location: /cita');
+                        }
+                       
+                    }
+
+                }else{
+                    Usuario::setAlerta('error','Usuario no encontrado');
+                }
+            }
+        }
+
+        $alertas = Usuario::getAlertas();
        
-        $router->render('auth/login', []);
+        $router->render('auth/login', [
+            'alertas' => $alertas
+        ]);
     }
 
     public static function logout(){
--- models/Usuario.php
+++ models/Usuario.php
@@ -54,6 +54,18 @@ class Usuario extends ActiveRecord {
         return self::$alertas;
     }
 
+     //mensajes de validación
+    public function validarLogin(){
+        if(!$this->email){
+            self::$alertas['error'][] = "El Email es Obligatorio";
+        }
+        if(!$this->password){
+            self::$alertas['error'][] = "El Password es Obligatorio";
+        }
+
+        return self::$alertas;
+    }
+
     public function existeUsuario(){
         $query = "SELECT * FROM " . self::$tabla . " WHERE email = '" . $this->email . "' LIMIT 1;";
         $resultado = self::$db->query($query);
@@ -73,5 +85,14 @@ class Usuario extends ActiveRecord {
         $this->token = uniqid();
     }
 
+    public function comprobarPasswordAndVerificado($password){
+        $resultado = password_verify($password, $this->password);
+        if(!$resultado || !$this->confirmado){
+            self::$alertas['error'][] = 'Password Incorrecto o tu cuenta no ha sido confirmada';
+        }else{
+            return true;
+        }
+        
+    }
 
 }
