d5a6161bf72e672c38e99cb62b19405878eab678
Mariana <marianaongay.01@gmail.com>
2025-09-01 13:31:01 -0600

    Nombre cliente y cerrar sesión

--- views/cita/index.php
+++ views/cita/index.php
@@ -1,6 +1,11 @@
 <h1 class="nombre-pagina">Crear Nueva Cita</h1>
 <p class="descripcion-pagina">Elige tus servicios y coloca tus datos</p>
 
+<div class="barra">
+    <p>Hola: <?php echo $nombre ?? '' ?></p>
+    <a class="boton" href="/logout">Cerrar Sesión</a>
+</div>
+
 <div id="app">
 
     <nav class="tabs">
